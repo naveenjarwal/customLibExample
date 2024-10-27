e98a9e5a9f75c1cc532ed5494c7f2eace93fcd8a
naveenjarwal <nvnjarwal@gmail.com>
2024-10-28 00:16:46 +0530

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const MyLibraryComponent = () => (
+  <View style={{ padding: 10 }}>
+    <Text>Hello from My Custom Library!</Text>
+  </View>
+);
